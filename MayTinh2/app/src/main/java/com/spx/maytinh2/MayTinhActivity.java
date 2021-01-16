b12aa3a88f1b09ab661d6ddb3468f4db6d824552
sondt32 <sondt32@fpt.edu.vn>
2021-01-16 17:17:02 +0700

    BH5 - Update code chương trình máy tính

--- MayTinh2/app/src/main/java/com/spx/maytinh2/MayTinhActivity.java
+++ MayTinh2/app/src/main/java/com/spx/maytinh2/MayTinhActivity.java
@@ -3,6 +3,7 @@ package com.spx.maytinh2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -15,51 +16,50 @@ public class MayTinhActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_may_tinh);
+    }
 
-        final Button button_7 = findViewById(R.id.btn_7);
-        button_7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(phepToan.length() ==0){
-                    // nhập vào số a
-                    so_a = Double.parseDouble(button_7.getText().toString());
+    public void NumberClick(View view){
+        Button btn = (Button)view;
+        if(this.phepToan.length() ==0){
+            // nhập vào số a
+            so_a = Double.parseDouble(btn.getText().toString());
+        }else{
+            so_b = Double.parseDouble(btn.getText().toString());
+        }
+    }
 
-                }else{
-                    so_b = Double.parseDouble(button_7.getText().toString());
-                }
+    public void ChonPhepToan(View view){
+        Button btn = (Button)view;  // Chuyển kiểu view thành nút bấm
+        this.phepToan = btn.getText().toString(); // lấy ký tự trên nút bấm gán vào biến
+        Log.d("aaa", this.phepToan);
+    }
+
+    public void KetQua(View view){
+        if(this.phepToan.length()>0){
+            Double KQ ;
+            switch (this.phepToan){
+                case "+":
+                    KQ = so_a + so_b;
+                    Toast.makeText(getBaseContext(), String.valueOf(KQ), Toast.LENGTH_SHORT).show();
+                    break;
+                ///....
+                case "-":
+                    KQ = so_a - so_b;
+                    Toast.makeText(getBaseContext(), String.valueOf(KQ), Toast.LENGTH_SHORT).show();
+                    break;
+                case "x":
+                    KQ = so_a * so_b;
+                    Toast.makeText(getBaseContext(), String.valueOf(KQ), Toast.LENGTH_SHORT).show();
+                    break;
+                case ":":
+                    KQ = so_a / so_b;
+                    Toast.makeText(getBaseContext(), String.valueOf(KQ), Toast.LENGTH_SHORT).show();
+                    break;
             }
-        });
-        //================= nút bấm cộng
-        final Button btn_cong = findViewById(R.id.btn_cong);
-        btn_cong.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                phepToan = btn_cong.getText().toString();
-            }
-        });
+        }
+    }
 
-        //==== Tính toán
-        Button btn_kq = findViewById(R.id.btn_kq);
-        btn_kq.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(phepToan.length()>0){
-                    Double KQ ;
-                    switch (phepToan){
-                        case "+":
-                            KQ = so_a + so_b;
-                            Toast.makeText(getBaseContext(), String.valueOf(KQ), Toast.LENGTH_SHORT).show();
-                            break;
-                            ///....
-                    }
-
-
-                }
-            }
-        });
-
-
-
-
+    public void Reset(View view){
+        this.phepToan = "";
     }
 }
