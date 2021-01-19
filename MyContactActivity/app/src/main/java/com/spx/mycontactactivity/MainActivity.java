fc1bb82c1840b114c03785f22b47e112d282cc2b
sondt32 <sondt32@fpt.edu.vn>
2021-01-19 14:23:57 +0700

    BH7 - List view có thêm phần sự kiện bấm vào 1 dòng thì Toast thông tin, bấm và giữ lâu thì sẽ xóa phần tử đó.

--- MyContactActivity/app/src/main/java/com/spx/mycontactactivity/MainActivity.java
+++ MyContactActivity/app/src/main/java/com/spx/mycontactactivity/MainActivity.java
@@ -3,7 +3,10 @@ package com.spx.mycontactactivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -25,6 +28,30 @@ public class MainActivity extends AppCompatActivity {
         listPersonAdapter = new ListPersonAdapter(list_person);
         listView = findViewById(R.id.lv_contact);
         listView.setAdapter(listPersonAdapter);
+        //==================================================
+        // sự kiện bấm chuột vào item
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
+                Person ps  = (Person) list_person.get(position);  // lấy thông tin phần tử ở dòng được bấm vào
+
+                Toast.makeText(getBaseContext(), ps.Name + " --- " + ps.Phone,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // sự kiện bấm giữ lâu sẽ xóa
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                list_person.remove(position);  // xóa phần tử ở vị trí tương ứng trong ArrayList
+
+                listPersonAdapter.notifyDataSetChanged(); // Báo cho adapter biết có sự thay đổi dữ liệu nguồn, adapter sẽ cập nhật lại danh sách và hiển thị lại
+
+                Toast.makeText(getBaseContext(), "1 row Deleted",Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
     }
 }
